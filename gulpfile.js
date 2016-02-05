17e8f76d8b4de239d0bd7a1d6282a065f849a55f
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2016-02-04 22:57:00 -0500

    Automated source code linting via Gulp.
    Gulp default task now runs JSHint, then Jasmine specs if JSHint passes.
    Project README section for developers updated accordingly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jasmine = require('gulp-jasmine'),
+    lintTaskName = 'lint',
+    testTaskName = 'test';
 
-gulp.task('default', function() {
+gulp.task(lintTaskName, function() {
+    return gulp.src(['src/*.js', 'spec/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task(testTaskName, [lintTaskName], function() {
     return gulp.src('spec/cribbageCardSpec.js').pipe(jasmine());
 });
+
+gulp.task('default', [testTaskName]);
