56c525e3f09071a1b7e843c8f4802554790578e3
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2016-02-07 23:16:01 -0500

    Corrected JSHint config error & gulpfile warnings.
    Yes, JSHint is even checking the gulpfile for compliance with its good
    JS rules. :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    jasmine = require('gulp-jasmine'),
-    jsdoc = require("gulp-jsdoc"),
-    lintTaskName = 'lint',
-    testTaskName = 'test',
-    jsDocTaskName = 'jsDoc',
-    continuousTaskName = 'continuous';
+/* global require */
 
-gulp.task(lintTaskName, function() {
-    return gulp.src(['src/*.js', 'spec/*Spec.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'));
-});
+(function() {
+    "use strict";
 
-gulp.task(testTaskName, [lintTaskName], function() {
-    return gulp.src('spec/*Spec.js')
-        .pipe(jasmine());
-});
+    var gulp = require('gulp'),
+        jshint = require('gulp-jshint'),
+        jasmine = require('gulp-jasmine'),
+        jsdoc = require("gulp-jsdoc"),
+        lintTaskName = 'lint',
+        testTaskName = 'test',
+        jsDocTaskName = 'jsDoc',
+        continuousTaskName = 'continuous';
 
-gulp.task(jsDocTaskName, [testTaskName], function() {
-    return gulp.src('./src/*.js')
-        .pipe(jsdoc('./jsDoc'));
-});
+    gulp.task(lintTaskName, function() {
+        return gulp.src(['src/*.js', 'spec/*Spec.js'])
+            .pipe(jshint())
+            .pipe(jshint.reporter('default'))
+            .pipe(jshint.reporter('fail'));
+    });
 
-gulp.task(continuousTaskName, [jsDocTaskName], function() {
-    gulp.watch(['src/*.js', 'spec/*Spec.js'], [jsDocTaskName]);
-});
+    gulp.task(testTaskName, [lintTaskName], function() {
+        return gulp.src('spec/*Spec.js')
+            .pipe(jasmine());
+    });
 
-// doesn't seem to help
-//process.on('uncaughtException', function(e) {
-//    console.error(e.stack);
-//});
+    gulp.task(jsDocTaskName, [testTaskName], function() {
+        return gulp.src('./src/*.js')
+            .pipe(jsdoc('./jsDoc'));
+    });
 
-gulp.task('default', [jsDocTaskName]);
+    gulp.task(continuousTaskName, [jsDocTaskName], function() {
+        gulp.watch(['src/*.js', 'spec/*Spec.js'], [jsDocTaskName]);
+    });
+
+    gulp.task('default', [jsDocTaskName]);
+}());
