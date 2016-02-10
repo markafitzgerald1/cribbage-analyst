543a9a6147e880afcf70ffbe4f08fcb059aa1d8e
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2016-02-10 00:21:45 -0500

    Automated report + enforce of unit test coverage.
    This closes #7.

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,25 @@
     var gulp = require('gulp'),
         jshint = require('gulp-jshint'),
         jasmine = require('gulp-jasmine'),
+        cover = require('gulp-coverage'),
         jsdoc = require("gulp-jsdoc"),
         lintTaskName = 'lint',
         testTaskName = 'test',
         jsDocTaskName = 'jsDoc',
-        continuousTaskName = 'continuous';
+        coverageTaskName = 'coverage',
+        continuousTaskName = 'continuous',
+        sources = 'src/*.js',
+        specs = 'spec/*Spec.js';
 
     gulp.task(lintTaskName, function() {
-        return gulp.src(['src/*.js', 'spec/*Spec.js'])
+        return gulp.src([sources, specs])
             .pipe(jshint())
             .pipe(jshint.reporter('default'))
             .pipe(jshint.reporter('fail'));
     });
 
     gulp.task(testTaskName, [lintTaskName], function() {
-        return gulp.src('spec/*Spec.js')
+        return gulp.src(specs)
             .pipe(jasmine());
     });
 
@@ -29,9 +33,21 @@
             .pipe(jsdoc('./jsDoc'));
     });
 
-    gulp.task(continuousTaskName, [jsDocTaskName], function() {
-        gulp.watch(['src/*.js', 'spec/*Spec.js'], [jsDocTaskName]);
+    gulp.task(coverageTaskName, [jsDocTaskName], function() {
+        return gulp.src(specs)
+            .pipe(cover.instrument({
+                pattern: [sources]
+            }))
+            .pipe(jasmine())
+            .pipe(cover.gather())
+            .pipe(cover.format())
+            .pipe(gulp.dest('coverage'))
+            .pipe(cover.enforce());
     });
 
-    gulp.task('default', [jsDocTaskName]);
+    gulp.task(continuousTaskName, [coverageTaskName], function() {
+        gulp.watch([sources, specs], [coverageTaskName]);
+    });
+
+    gulp.task('default', [coverageTaskName]);
 }());
