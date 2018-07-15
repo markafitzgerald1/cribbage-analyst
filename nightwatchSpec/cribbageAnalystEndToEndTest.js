2c53225dedfa137650f3bf8d363558682011562a
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2018-07-15 16:34:46 -0400

    Allow new larger maximum e2e test times
    Updated to highest values seen in a handful of runs on the slowest
    computer builds are known to run on: my personal laptop in power saving
    mode.

--- nightwatchSpec/cribbageAnalystEndToEndTest.js
+++ nightwatchSpec/cribbageAnalystEndToEndTest.js
@@ -9,10 +9,10 @@
 
     const siteUrl = 'http://localhost:8080',
         inputSelector = 'input[type=text]',
-        initialRenderTimeoutMilliseconds = 123,
+        initialRenderTimeoutMilliseconds = 653,
         analysesSelector = '#analyses',
         firstAnalysisSelector = '.analysis:first-of-type',
-        analysisTimeoutMilliseconds = 54;
+        analysisTimeoutMilliseconds = 179;
 
     /* Create at least one e2e test verifying entry point + HTML with Vue.js
      * markup works as expected - at least for a happy path. */
