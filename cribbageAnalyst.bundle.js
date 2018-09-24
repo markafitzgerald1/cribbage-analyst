442ca07d6d417bdb682e9cb634a087a62dc5bab5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-24 02:32:53 +0000

    Deploy markafitzgerald1/cribbage-analyst to github.com/markafitzgerald1/cribbage-analyst.git:gh-pages

--- cribbageAnalyst.bundle.js
+++ cribbageAnalyst.bundle.js
@@ -1018,7 +1018,7 @@ var cribbageAnalyst =
 	  var undefined;
 
 	  /** Used as the semantic version number. */
-	  var VERSION = '4.17.10';
+	  var VERSION = '4.17.11';
 
 	  /** Used as the size to enable large array optimizations. */
 	  var LARGE_ARRAY_SIZE = 200;
@@ -1282,7 +1282,7 @@ var cribbageAnalyst =
 	  var reHasUnicode = RegExp('[' + rsZWJ + rsAstralRange  + rsComboRange + rsVarRange + ']');
 
 	  /** Used to detect strings that need a more robust regexp to match words. */
-	  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2,}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
+	  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
 
 	  /** Used to assign default `context` object properties. */
 	  var contextProps = [
@@ -2228,20 +2228,6 @@ var cribbageAnalyst =
 	      }
 	    }
 	    return result;
-	  }
-
-	  /**
-	   * Gets the value at `key`, unless `key` is "__proto__".
-	   *
-	   * @private
-	   * @param {Object} object The object to query.
-	   * @param {string} key The key of the property to get.
-	   * @returns {*} Returns the property value.
-	   */
-	  function safeGet(object, key) {
-	    return key == '__proto__'
-	      ? undefined
-	      : object[key];
 	  }
 
 	  /**
@@ -4701,7 +4687,7 @@ var cribbageAnalyst =
 	          if (isArguments(objValue)) {
 	            newValue = toPlainObject(objValue);
 	          }
-	          else if (!isObject(objValue) || (srcIndex && isFunction(objValue))) {
+	          else if (!isObject(objValue) || isFunction(objValue)) {
 	            newValue = initCloneObject(srcValue);
 	          }
 	        }
@@ -7622,6 +7608,22 @@ var cribbageAnalyst =
 	        array[length] = isIndex(index, arrLength) ? oldArray[index] : undefined;
 	      }
 	      return array;
+	    }
+
+	    /**
+	     * Gets the value at `key`, unless `key` is "__proto__".
+	     *
+	     * @private
+	     * @param {Object} object The object to query.
+	     * @param {string} key The key of the property to get.
+	     * @returns {*} Returns the property value.
+	     */
+	    function safeGet(object, key) {
+	      if (key == '__proto__') {
+	        return;
+	      }
+
+	      return object[key];
 	    }
 
 	    /**
