8cd53a75e975d6e412bd22feef4ecf654fcb6beb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 23:58:41 +0000

    Deploy markafitzgerald1/cribbage-analyst to github.com/markafitzgerald1/cribbage-analyst.git:gh-pages

--- cribbageAnalyst.bundle.js
+++ cribbageAnalyst.bundle.js
@@ -1008,7 +1008,7 @@ var cribbageAnalyst =
 	var __WEBPACK_AMD_DEFINE_RESULT__;/* WEBPACK VAR INJECTION */(function(global, module) {/**
 	 * @license
 	 * Lodash <https://lodash.com/>
-	 * Copyright JS Foundation and other contributors <https://js.foundation/>
+	 * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
 	 * Released under MIT license <https://lodash.com/license>
 	 * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
 	 * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -1019,7 +1019,7 @@ var cribbageAnalyst =
 	  var undefined;
 
 	  /** Used as the semantic version number. */
-	  var VERSION = '4.17.11';
+	  var VERSION = '4.17.13';
 
 	  /** Used as the size to enable large array optimizations. */
 	  var LARGE_ARRAY_SIZE = 200;
@@ -3678,16 +3678,10 @@ var cribbageAnalyst =
 	        value.forEach(function(subValue) {
 	          result.add(baseClone(subValue, bitmask, customizer, subValue, value, stack));
 	        });
-
-	        return result;
-	      }
-
-	      if (isMap(value)) {
+	      } else if (isMap(value)) {
 	        value.forEach(function(subValue, key) {
 	          result.set(key, baseClone(subValue, bitmask, customizer, key, value, stack));
 	        });
-
-	        return result;
 	      }
 
 	      var keysFunc = isFull
@@ -4611,8 +4605,8 @@ var cribbageAnalyst =
 	        return;
 	      }
 	      baseFor(source, function(srcValue, key) {
+	        stack || (stack = new Stack);
 	        if (isObject(srcValue)) {
-	          stack || (stack = new Stack);
 	          baseMergeDeep(object, source, key, srcIndex, baseMerge, customizer, stack);
 	        }
 	        else {
@@ -6429,7 +6423,7 @@ var cribbageAnalyst =
 	      return function(number, precision) {
 	        number = toNumber(number);
 	        precision = precision == null ? 0 : nativeMin(toInteger(precision), 292);
-	        if (precision) {
+	        if (precision && nativeIsFinite(number)) {
 	          // Shift with exponential notation to avoid floating-point issues.
 	          // See [MDN](https://mdn.io/round#Examples) for more details.
 	          var pair = (toString(number) + 'e').split('e'),
@@ -7612,7 +7606,7 @@ var cribbageAnalyst =
 	    }
 
 	    /**
-	     * Gets the value at `key`, unless `key` is "__proto__".
+	     * Gets the value at `key`, unless `key` is "__proto__" or "constructor".
 	     *
 	     * @private
 	     * @param {Object} object The object to query.
@@ -7620,6 +7614,10 @@ var cribbageAnalyst =
 	     * @returns {*} Returns the property value.
 	     */
 	    function safeGet(object, key) {
+	      if (key === 'constructor' && typeof object[key] === 'function') {
+	        return;
+	      }
+
 	      if (key == '__proto__') {
 	        return;
 	      }
@@ -11420,6 +11418,7 @@ var cribbageAnalyst =
 	          }
 	          if (maxing) {
 	            // Handle invocations in a tight loop.
+	            clearTimeout(timerId);
 	            timerId = setTimeout(timerExpired, wait);
 	            return invokeFunc(lastCallTime);
 	          }
@@ -15806,9 +15805,12 @@ var cribbageAnalyst =
 	      , 'g');
 
 	      // Use a sourceURL for easier debugging.
+	      // The sourceURL gets injected into the source that's eval-ed, so be careful
+	      // with lookup (in case of e.g. prototype pollution), and strip newlines if any.
+	      // A newline wouldn't be a valid sourceURL anyway, and it'd enable code injection.
 	      var sourceURL = '//# sourceURL=' +
-	        ('sourceURL' in options
-	          ? options.sourceURL
+	        (hasOwnProperty.call(options, 'sourceURL')
+	          ? (options.sourceURL + '').replace(/[\r\n]/g, ' ')
 	          : ('lodash.templateSources[' + (++templateCounter) + ']')
 	        ) + '\n';
 
@@ -15841,7 +15843,9 @@ var cribbageAnalyst =
 
 	      // If `variable` is not specified wrap a with-statement around the generated
 	      // code to add the data object to the top of the scope chain.
-	      var variable = options.variable;
+	      // Like with sourceURL, we take care to not check the option's prototype,
+	      // as this configuration is a code injection vector.
+	      var variable = hasOwnProperty.call(options, 'variable') && options.variable;
 	      if (!variable) {
 	        source = 'with (obj) {\n' + source + '\n}\n';
 	      }
@@ -18046,10 +18050,11 @@ var cribbageAnalyst =
 	    baseForOwn(LazyWrapper.prototype, function(func, methodName) {
 	      var lodashFunc = lodash[methodName];
 	      if (lodashFunc) {
-	        var key = (lodashFunc.name + ''),
-	            names = realNames[key] || (realNames[key] = []);
-
-	        names.push({ 'name': methodName, 'func': lodashFunc });
+	        var key = lodashFunc.name + '';
+	        if (!hasOwnProperty.call(realNames, key)) {
+	          realNames[key] = [];
+	        }
+	        realNames[key].push({ 'name': methodName, 'func': lodashFunc });
 	      }
 	    });
 
